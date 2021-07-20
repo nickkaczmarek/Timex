d81c809328b36eac45415bf924f318555e07e1e9
Nick Kaczmarek <nickkaczmarek@me.com>
2021-07-20 13:46:49 -0500

    Initial Commit

---
+++ Timex/TimexApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimexApp.swift
+//  Timex
+//
+//  Created by Nick Kaczmarek on 7/20/21.
+//
+
+import SwiftUI
+
+@main
+struct TimexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
