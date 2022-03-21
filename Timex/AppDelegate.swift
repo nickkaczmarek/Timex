24e2c3ecead88ee38c79af5bce8a52f2a63008a1
Nick Kaczmarek <nickkaczmarek@me.com>
2022-03-21 10:15:18 -0500

    [main] - Adds icons and webview to window time tracking system. - nk

---
+++ Timex/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  Timex
+//
+//  Created by Nick Kaczmarek on 8/23/21.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+}
--- Timex/ContentView.swift
+++ Timex/ContentView.swift
@@ -8,9 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let timexUrl = "https://timex.apps.wwt.com"
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        SafariWebView(mesgURL: timexUrl)
+            .frame(minWidth: 400,
+                   idealWidth: 1000,
+                   maxWidth: .infinity,
+                   minHeight: 500,
+                   idealHeight: 800,
+                   maxHeight: .infinity,
+                   alignment: .center)
+    }
+}
+
+struct SafariWebView: View {
+    @ObservedObject var model: WebViewModel
+
+    init(mesgURL: String) {
+        //Assign the url to the model and initialise the model
+        self.model = WebViewModel(link: mesgURL)
+    }
+    
+    var body: some View {
+        //Create the WebView with the model
+        NSWKWebView(viewModel: model)
     }
 }
 
